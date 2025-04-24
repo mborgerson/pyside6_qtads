1accb560deba438a4d1487b28f141b764f319a57
Matt Borgerson <contact@mborgerson.com>
2025-04-24 16:25:44 -0700

    Whitelist 6.9 libs for auditwheel

--- scripts/custom-auditwheel.py
+++ scripts/custom-auditwheel.py
@@ -13,12 +13,14 @@ if __name__ == "__main__":
         policies = json.loads(POLICY_JSON_MAP[libc].read_text())
         for p in policies:
             p['lib_whitelist'].extend([
+                'libpyside6.abi3.so.6.9',
                 'libpyside6.abi3.so.6.8',
                 'libpyside6.abi3.so.6.7',
                 'libpyside6.abi3.so.6.6',
                 'libpyside6.abi3.so.6.5',
                 'libpyside6.abi3.so.6.4',
                 'libpyside6.abi3.so.6.3',
+                'libshiboken6.abi3.so.6.9',
                 'libshiboken6.abi3.so.6.8',
                 'libshiboken6.abi3.so.6.7',
                 'libshiboken6.abi3.so.6.6',
@@ -27,6 +29,7 @@ if __name__ == "__main__":
                 'libshiboken6.abi3.so.6.3',
                 'libQt6Widgets.so.6',
                 'libQt6Gui.so.6',
+                'libpyside6qml.abi3.so.6.9',
                 'libpyside6qml.abi3.so.6.8',
                 'libpyside6qml.abi3.so.6.7',
                 'libpyside6qml.abi3.so.6.6',
